3f65cda51919ddcf77db0668c81ebea9c09ffe08
Unknown <elian.hamon@viacesi.fr>
2020-08-29 18:12:11 +0200

    update exemple

--- examples/basicPin.lua
+++ examples/basicPin.lua
@@ -1,6 +1,6 @@
 package.loadlib("/usr/lib/libLua-GPIO.so", "luaopen_luagpio")() -- load the library. Functions are accessible via luagpio.<function name>()
 
-luagpio.initPin(17, "output") -- set the pin to output
+luagpio.initPin(17, 1) -- set the pin to output
 
 luagpio.setPin(17, 1) -- switch on the pin
 
