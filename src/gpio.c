82b14025d205e82393d71be64fd0514fbac08dc1
Stovent <elian.hamon@viacesi.fr>
2024-06-12 15:48:46 -0400

    Add brief documentation and remove LUTs

--- src/gpio.c
+++ src/gpio.c
@@ -3,57 +3,36 @@
 #include <sys/mman.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <string.h>
-#include <stdio.h>
 #include <unistd.h>
 
-#define PERIPHERALS_BASE_ADDR (0x20200000)
-#define MAP_SIZE (0xA0)
+#include <string.h>
+#include <stdio.h>
 
-static uint32_t* gpio = NULL;
+#define PERIPHERALS_BASE_ADDR (0x20200000)
+
+static volatile uint32_t* gpio = NULL;
 static int fdgpio = -1;
 
-static const int fselShift[] = {
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9,
+enum GPIOPeripherals
+{
+    GPFSEL0 = 0x00,
+    // GPFSEL1 = 0x04,
+    // GPFSEL2 = 0x08,
+    // GPFSEL3 = 0x0C,
+    // GPFSEL4 = 0x10,
+    // GPFSEL5 = 0x14,
+
+    GPSET0 = 0x1C,
+    // GPSET1 = 0x20,
+
+    GPCLR0 = 0x28,
+    // GPCLR1 = 0x2C,
+
+    GPLEV0 = 0x34,
+    // GPLEV1 = 0x38,
+
+    GPEND = 0xA0, // End address
 };
-
-static const int fsel[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    5, 5, 5, 5,
-};
-
-static const int set[] = {
-    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-    8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-};
-
-static const int setShift[] = {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-};
-
-static const int clr[] = {
-    10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-    11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-};
-
-static const int* const clrShift = setShift;
-
-static const int lvl[] = {
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
-};
-
-static const int* const lvlShift = setShift;
 
 bool gpio_init()
 {
@@ -63,45 +42,46 @@ bool gpio_init()
         return false;
     }
 
-    if((gpio = mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fdgpio, PERIPHERALS_BASE_ADDR)) == MAP_FAILED)
+    if((gpio = mmap(NULL, GPEND, PROT_READ | PROT_WRITE, MAP_SHARED, fdgpio, PERIPHERALS_BASE_ADDR)) == MAP_FAILED)
     {
         fprintf(stderr, "Could not map gpio memory: %s\n", strerror(errno));
         return false;
     }
 
-//    fputs("GPIO initialized\n", stdout);
     return true;
 }
 
 void gpio_exit()
 {
-    munmap(gpio, MAP_SIZE);
+    munmap((void*)gpio, GPEND); // Cast to avoid volatile discard warning.
     close(fdgpio);
 }
 
+#define FSEL_REG(pin) (pin / 10)
+#define FSEL_BITS(pin) ((pin % 10) * 3)
+
 void gpio_init_pin(const uint8_t pin, const bool output)
 {
-    *(gpio + fsel[pin]) &= ~(1 << fselShift[pin]);
-    *(gpio + fsel[pin]) |= (output << fselShift[pin]);
+    const uint8_t addr = GPFSEL0 + FSEL_REG(pin);
+    const uint8_t shift = FSEL_BITS(pin);
 
-//    fprintf(stdout, "Pin %d set to %s\n", pin, output ? "output" : "input");
+    uint32_t reg = gpio[addr];
+    reg &= ~(7 << shift);
+    reg |= (output << shift);
+    gpio[addr] = reg;
 }
 
-void gpio_set_pin(const uint8_t pin, const bool value)
-{
-    if(value)
-        *(gpio + set[pin]) |=  (1 << setShift[pin]);
-    else
-        *(gpio + clr[pin]) |=  (1 << clrShift[pin]);
+#define SET_CLR_LVL_REG(pin) (pin >> 5)
+#define SET_CLR_LVL_BIT(pin) (pin & 0x1Fu)
 
-//    fprintf(stdout, "Set pin %d to value %d\n", pin, value);
+void gpio_set_pin(const uint8_t pin, const bool high)
+{
+    const uint8_t addr = high ? GPSET0 : GPCLR0;
+
+    gpio[addr + SET_CLR_LVL_REG(pin)] = (1 << SET_CLR_LVL_BIT(pin));
 }
 
 bool gpio_get_pin(const uint8_t pin)
 {
-    const bool value = *(gpio + lvl[pin]) & (1 << lvlShift[pin]);
-
-//    fprintf(stdout, "Get pin %d at value %d\n", pin, value);
-
-    return value;
+    return gpio[GPLEV0 + SET_CLR_LVL_REG(pin)] >> SET_CLR_LVL_BIT(pin) & 1;
 }
--- src/gpio.h
+++ src/gpio.h
@@ -1,13 +1,37 @@
+/** @file gpio.h
+ * @brief Small library to control the GPIO of a Raspberry Pi.
+ */
+
 #ifndef GPIO_H
 #define GPIO_H
 
 #include <stdbool.h>
 #include <stdint.h>
 
+/** @brief Init the library by mapping the GPIO memory.
+ * @return true on success, false on error.
+ */
 bool gpio_init();
+
+/** @brief Releases the mapped memory after usage. */
 void gpio_exit();
-void gpio_init_pin(const uint8_t pin, const bool output);
-void gpio_set_pin(const uint8_t pin, const bool value);
-bool gpio_get_pin(const uint8_t pin);
+
+/** @brief Inits the pin to input or output
+ * @param pin The GPIO pin number.
+ * @param output true to set as output, false as input.
+ */
+void gpio_init_pin(uint8_t pin, bool output);
+
+/** @brief Sets the pin to high or low.
+ * @param pin the GPIO pin number.
+ * @param high true to set to high, false for low.
+ */
+void gpio_set_pin(uint8_t pin, bool high);
+
+/** @brief Returns the state of the pin.
+ * @param pin the GPIO pin number.
+ * @return true when the pin is high, false when low.
+ */
+bool gpio_get_pin(uint8_t pin);
 
 #endif // GPIO_H
